7450987388bf1cd77d887aeaaef520d254c53e53
Krystof Hruby <hrubyk97@gmail.com>
2020-09-02 13:12:18 +0200

    Deleted comments with functionality originally from osm_parser.cpp

--- backend/src/osm_parser/osm_parser.cpp
+++ backend/src/osm_parser/osm_parser.cpp
@@ -23,145 +23,11 @@
 #include <osmium/handler/node_locations_for_ways.hpp>
 #include <osmium/geom/wkt.hpp>
 
-
 using namespace std;
 // const string kInputPath = "/home/hrubyk/Downloads/czech-republic-latest.osm.pbf";
 const string kInputPath = "/home/hrubyk/Downloads/luxembourg-latest.osm.pbf";
 const string kOutputPath = "/home/hrubyk/projects/maps/backend/graph.sql";
 static size_t size_tDefault;
-// using index_type = osmium::index::map::SparseMemMap<osmium::unsigned_object_id_type, size_t>;
-
-/*
-class Writer {
-    ofstream f_;
-public:
-    Writer(const string & file_name) {
-        f_ = ofstream{};
-        f_.open(file_name);
-    }
-
-    ~Writer() {
-        f_.close();
-    }
-
-    void WriteCreateTableSql(const string& table_name) {
-        string sql = "CREATE TABLE " + table_name + "("  \
-        "id serial   NOT NULL," \
-        "osm_id INT   NOT NULL," \
-        "geog geography(LINESTRING)    NOT NULL," \
-        "from INT     NOT NULL," \
-        "to INT    NOT NULL;";
-        f_ << sql << std::endl;
-    }
-
-    void WriteCreateInsertSql(const string & table_name, const string & osm_id, const string & geog, const string & from, const string & to) {
-        string sql = "INSERT INTO " + table_name + " (osm_id, geog, from, to) "  \
-         "VALUES (" + osm_id + ", " + geog + ", " + from + ", " + to + " );";
-        f_ << sql << std::endl;
-    }
-
-
-};
-*/
-/*
-
-template <typename GeomFactory>
-class GraphGenerator : public osmium::handler::Handler {
-    index_type & nodes_ptr_;
-    GeomFactory factory_;
-    Writer writer_;
-
-    using const_nodelist_iterator = osmium::WayNodeList::const_iterator;
-
-public:
-
-    GraphGenerator(index_type & index_ptr, const GeomFactory & factory) : nodes_ptr_(index_ptr), factory_{factory}, writer_{kOutputPath} {}
-
-    void way(const osmium::Way& way) {
-        // Skip all ways that are not roads.
-        if (way.tags().get_value_by_key("highway") == nullptr) {
-            return;
-        }
-
-        // std::cout << "way " << way.id() << '\n';
-
-        // std::cout << "Node_index_size "<< nodes_ptr_.size() << std::endl;
-        const osmium::WayNodeList & nodes = way.nodes();
-        // std::cout << "nodes size " << nodes.size() << '\n';
-        if (nodes.size() < 2) {
-            std::cout << "Way consists of less than 2 points." << std::endl;
-            throw 2;
-        }
-        if (way.id() == 10903355) {
-            std::cout << "HELLO\n";
-            for(auto&& nr : nodes) {
-                std::cout << nr.positive_ref() << " " << nr.ref() << " ";
-            }
-            std::cout << std::endl;
-        }
-        const_nodelist_iterator it1 = nodes.cbegin();
-        const_nodelist_iterator it2 = nodes.cend();
-        SaveEdge(it1, it2);
-
-        const_nodelist_iterator first = nodes.cbegin();
-        const_nodelist_iterator second = nodes.cbegin();
-        ++second;
-        size_t i = 0;
-        for(; second != nodes.cend(); ++second) {
-            // Skip invalid nodes. - TOOD what is such a node is a first or last one???
-            // if (second->ref() < 0) continue;
-
-            size_t value = nodes_ptr_.get_noexcept(second->positive_ref());
-            bool not_in_index = value == osmium::index::empty_value<size_t>();
-            // All of these nodes should definitely be in the index.
-            if (not_in_index) {
-                std::cout << "Node not found in link index??" << std::endl;
-                std::cout << second->positive_ref() << std::endl;
-                throw 1;
-            }
-            bool is_intersection = value > 1 && value != osmium::index::empty_value<size_t>();
-            if (is_intersection) {
-                const_nodelist_iterator to = second;
-                // Incrementing `to` since we include the intersection point in the linestring.
-                SaveEdge(first, ++to);
-                first = second;
-            }
-            ++i;
-        }
-
-        // Create a way segment from first to the last point in `nodes`.
-        // If it is an intersection, it was added in the for loop.
-        // In that case `first` should point to the last point.
-        // If the last one was not intersection then create
-        // way segment.
-        const_nodelist_iterator it = first;
-        if (++it != nodes.cend()) {
-            SaveEdge(first, second);
-        }
-        first = second;
-    }
-
-private:
-
-    void SaveEdge(const_nodelist_iterator & from, const_nodelist_iterator & to) {
-        auto&& linestring = CreateLineString(from, to);
-        // std::cout << linestring << std::endl;
-        // Write sql command to insert row.
-        //writer_.WriteCreateInsertSql()
-    }
-
-    std::string CreateLineString(const_nodelist_iterator from, const_nodelist_iterator to) {
-        factory_.linestring_start();
-        size_t point_count = factory_.fill_linestring_unique(from, to);
-        if (point_count < 2) {
-            std::cout << "Way segment consists of less than 2 points." << std::endl;
-            //throw 2;
-        }
-        return factory_.linestring_finish(point_count);
-    }
-};
-
-*/
 
 void CalculateNodeLinks(index_type & node_index) {
     auto otypes = osmium::osm_entity_bits::way; //osmium::osm_entity_bits::node |
@@ -176,21 +42,6 @@ void CalculateNodeLinks(index_type & node_index) {
 
 
 int main() {
-/*
-    auto otypes = osmium::osm_entity_bits::node | osmium::osm_entity_bits::way;
-    osmium::io::Reader reader{"input.osm.pbf", otypes};
-
-    namespace map = osmium::index::map;
-    using location index_type = map::SparseMemArray<osmium::unsigned_object_id_type, osmium::Location>;
-    using location_handler_type = osmium::handler::NodeLocationsForWays<index_type>;
-
-    location_index_type location_index;
-    location_handler_type location_handler{index};
-
-    osmium::apply(reader, location_handler, handler);
-    reader.close();
-*/
-
     auto otypes = osmium::osm_entity_bits::node | osmium::osm_entity_bits::way;
     osmium::io::Reader reader{kInputPath, otypes};
     string index_type_name = "sparse_mem_map";
--- backend/src/osm_parser/writer.cpp
+++ backend/src/osm_parser/writer.cpp
@@ -3,38 +3,7 @@
 //
 #include "writer.h"
 using namespace std;
-/*
-class Writer {
-    ofstream f_;
-public:
-    Writer(const string & file_name) {
-        f_ = ofstream{};
-        f_.open(file_name);
-    }
 
-    ~Writer() {
-        f_.close();
-    }
-
-    void WriteCreateTableSql(const string& table_name) {
-        string sql = "CREATE TABLE " + table_name + "("  \
-        "id serial   NOT NULL," \
-        "osm_id INT   NOT NULL," \
-        "geog geography(LINESTRING)    NOT NULL," \
-        "from INT     NOT NULL," \
-        "to INT    NOT NULL;";
-        f_ << sql << std::endl;
-    }
-
-    void WriteCreateInsertSql(const string & table_name, const string & osm_id, const string & geog, const string & from, const string & to) {
-        string sql = "INSERT INTO " + table_name + " (osm_id, geog, from, to) "  \
-         "VALUES (" + osm_id + ", " + geog + ", " + from + ", " + to + " );";
-        f_ << sql << std::endl;
-    }
-
-
-};
-*/
 Writer::Writer(const string & file_name) {
     f_ = ofstream{};
     f_.open(file_name);
