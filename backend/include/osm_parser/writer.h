58244fdf0699c8875df3c3afa461fd12d0aa481d
Krystof Hruby <hrubyk97@gmail.com>
2021-04-22 19:46:10 +0200

    namespace indent fixed.

--- backend/include/osm_parser/writer.h
+++ backend/include/osm_parser/writer.h
@@ -15,97 +15,101 @@
 #include <osmium/handler/node_locations_for_ways.hpp>
 #include <osmium/geom/wkt.hpp>
 namespace osm_parser {
+    
+/**
+ * Interface for osm_parser writers that can be used
+ * to write a sql script to a file that generates an edgelist.
+ *
+ * A part of the script should also be to generate an geo index
+ * over the edgelist based on the geometries and adding length
+ * column as well as counting lengths for edges.
+ */
+class IWriter {
+public:
     /**
-     * Interface for osm_parser writers that can be used
-     * to write a sql script to a file that generates an edgelist.
-     *
-     * A part of the script should also be to generate an geo index
-     * over the edgelist based on the geometries and adding length
-     * column as well as counting lengths for edges.
+     * Write any sql script initialization before edges are written to a file.
+     * @param table_name Table name that will hold the data in db.
      */
-    class IWriter {
-    public:
-        /**
-         * Write any sql script initialization before edges are written to a file.
-         * @param table_name Table name that will hold the data in db.
-         */
-        virtual void WriteInitSql(const std::string& table_name) = 0;
-
-        /**
-         * Save edge to a file in such a form that it can be loaded to db later
-         * when the sql script is invoked.
-         * @param table_name Table name that will hold the data in db.
-         * @param edge Edge to be saved to file.
-         */
-        virtual void WriteEdge(const std::string& table_name, const Edge& edge) = 0;
-
-        /**
-         * Write any necessary finish for sql script.
-         * @param table_name Table name that will hold the data in db.
-         */
-        virtual void WriteFinishSql(const std::string& table_name) = 0;
-
-        virtual ~IWriter() {}
-    };
+    virtual void WriteInitSql(const std::string& table_name) = 0;
 
     /**
-     * CopyWriter generates a sql script that uses
-     * COPY to load edge data to db.
-     *
+     * Save edge to a file in such a form that it can be loaded to db later
+     * when the sql script is invoked.
+     * @param table_name Table name that will hold the data in db.
+     * @param edge Edge to be saved to file.
      */
-    class CopyWriter : public IWriter {
-        /**
-         * Ofstream used to write sql script to a file.
-         */
-        std::ofstream f_init_table_;
+    virtual void WriteEdge(const std::string& table_name, const Edge& edge) = 0;
 
-        /**
-         * Ofstream used to write edge data to a file.
-         */
-        std::ofstream f_data_;
+    /**
+     * Write any necessary finish for sql script.
+     * @param table_name Table name that will hold the data in db.
+     */
+    virtual void WriteFinishSql(const std::string& table_name) = 0;
 
-        /**
-         * Path of file with edge data.
-         */
-        std::string data_path_;
+    virtual ~IWriter() {}
+};
 
-    public:
-        /**
-         * @param sql_path Path and name of the file where sql script is saved.
-         * @param data_path Path and name of the file where edge data is saved.
-         */
-        CopyWriter(const std::string& sql_path, const std::string& data_path);
+/**
+ * CopyWriter generates a sql script that uses
+ * COPY to load edge data to db.
+ *
+ */
+class CopyWriter : public IWriter {
+    /**
+     * Ofstream used to write sql script to a file.
+     */
+    std::ofstream f_init_table_;
 
-        ~CopyWriter() override;
+    /**
+     * Ofstream used to write edge data to a file.
+     */
+    std::ofstream f_data_;
 
-        /**
-         * Write whole sql script - Table creation, length column
-         * generation, length computation, geo index creation.
-         * @param table_name Table name that will hold the data in db.
-         */
-        void WriteInitSql(const std::string& table_name) override;
+    /**
+     * Path of file with edge data.
+     */
+    std::string data_path_;
 
-        /**
-         * Write edge to a edge data file.
-         * @param table_name Table name that will hold the data in db.
-         * @param edge Edge to be saved to file.
-         */
-        void WriteEdge(const std::string& table_name, const Edge& edge) override;
+public:
+    /**
+     * @param sql_path Path and name of the file where sql script is saved.
+     * @param data_path Path and name of the file where edge data is saved.
+     */
+    CopyWriter(const std::string& sql_path, const std::string& data_path);
 
-        /**
-         * No need to do anything since everyting can be doone in WriteInitSql.
-         * @param table_name Table name that will hold the data in db.
-         */
-        void WriteFinishSql(const std::string& table_name) override;
+    ~CopyWriter() override;
 
-    private:
-        std::string GetCreateEdgesTable(const std::string& table_name) const;
+    /**
+     * Write whole sql script - Table creation, length column
+     * generation, length computation, geo index creation.
+     * @param table_name Table name that will hold the data in db.
+     */
+    void WriteInitSql(const std::string& table_name) override;
 
-        std::string GetCreateVertexIdMappingTable(const std::string& edges_table, const std::string& mapping_table) const;
+    /**
+     * Write edge to a edge data file.
+     * @param table_name Table name that will hold the data in db.
+     * @param edge Edge to be saved to file.
+     */
+    void WriteEdge(const std::string& table_name, const Edge& edge) override;
 
-        std::string GetInsertToFinalTable(const std::string& final_table, const std::string& edges_table, const std::string& mapping_table) const;
+    /**
+     * No need to do anything since everyting can be doone in WriteInitSql.
+     * @param table_name Table name that will hold the data in db.
+     */
+    void WriteFinishSql(const std::string& table_name) override;
 
-        std::string GetDropTable(const std::string& table_name) const;
-    };
+private:
+    std::string GetCreateEdgesTable(const std::string& table_name) const;
+
+    std::string GetCreateVertexIdMappingTable(const std::string& edges_table, const std::string& mapping_table) const;
+
+    std::string GetInsertToFinalTable(const std::string& final_table, const std::string& edges_table, const std::string& mapping_table) const;
+
+    std::string GetDropTable(const std::string& table_name) const;
+};
+
+
+
 }
 #endif //BACKEND_WRITER_H
--- backend/src/osm_parser/writer.cpp
+++ backend/src/osm_parser/writer.cpp
@@ -1,88 +1,91 @@
 #include "osm_parser/writer.h"
 using namespace std;
 namespace osm_parser {
-    CopyWriter::CopyWriter(const std::string &sql_path, const std::string &data_path) {
-        f_init_table_ = ofstream{};
-        f_init_table_.open(sql_path);
-        f_data_ = ofstream{};
-        f_data_.open(data_path);
-        data_path_ = data_path;
-    }
+    
+CopyWriter::CopyWriter(const std::string &sql_path, const std::string &data_path) {
+    f_init_table_ = ofstream{};
+    f_init_table_.open(sql_path);
+    f_data_ = ofstream{};
+    f_data_.open(data_path);
+    data_path_ = data_path;
+}
 
-    CopyWriter::~CopyWriter() {
-        f_init_table_.close();
-        f_data_.close();
-    }
+CopyWriter::~CopyWriter() {
+    f_init_table_.close();
+    f_data_.close();
+}
 
-    void CopyWriter::WriteInitSql(const string& table_name) {
-        std::string temporary_edges_table = table_name + "_temp";
-        std::string vertex_id_mapping_table = temporary_edges_table + "_vertex_mapping_temp";
-        // Write COPY command that loads edges from data to table `table_name`.
-        string copy = "COPY " + temporary_edges_table + " FROM '" + data_path_ + "' DELIMITER ';' CSV; ";
-        // Add length column.
-        string add_length_column = "ALTER TABLE " + table_name + " ADD COLUMN length double precision; ";
-        // Calculate lengths.
-        string fill_length_column = "UPDATE " + table_name + " set length = st_length(geog); ";
-        // Create geo index.
-        string create_index = "CREATE INDEX " + table_name + "_gix ON " + table_name + " USING GIST (geog); ";
-        f_init_table_ << GetCreateEdgesTable(temporary_edges_table) << std::endl;
-        f_init_table_ << copy << std::endl;
-        f_init_table_ << GetCreateVertexIdMappingTable(temporary_edges_table, vertex_id_mapping_table) << std::endl;
-        f_init_table_ << GetCreateEdgesTable(table_name) << std::endl;
-        f_init_table_ << GetInsertToFinalTable(table_name, temporary_edges_table, vertex_id_mapping_table) << std::endl;
-        f_init_table_ << GetDropTable(temporary_edges_table) << std::endl;
-        f_init_table_ << GetDropTable(vertex_id_mapping_table) << std::endl;
-        f_init_table_ << add_length_column << std::endl;
-        f_init_table_ << fill_length_column << std::endl;
-        f_init_table_ << create_index << std::endl;
-    }
+void CopyWriter::WriteInitSql(const string& table_name) {
+    std::string temporary_edges_table = table_name + "_temp";
+    std::string vertex_id_mapping_table = temporary_edges_table + "_vertex_mapping_temp";
+    // Write COPY command that loads edges from data to table `table_name`.
+    string copy = "COPY " + temporary_edges_table + " FROM '" + data_path_ + "' DELIMITER ';' CSV; ";
+    // Add length column.
+    string add_length_column = "ALTER TABLE " + table_name + " ADD COLUMN length double precision; ";
+    // Calculate lengths.
+    string fill_length_column = "UPDATE " + table_name + " set length = st_length(geog); ";
+    // Create geo index.
+    string create_index = "CREATE INDEX " + table_name + "_gix ON " + table_name + " USING GIST (geog); ";
+    f_init_table_ << GetCreateEdgesTable(temporary_edges_table) << std::endl;
+    f_init_table_ << copy << std::endl;
+    f_init_table_ << GetCreateVertexIdMappingTable(temporary_edges_table, vertex_id_mapping_table) << std::endl;
+    f_init_table_ << GetCreateEdgesTable(table_name) << std::endl;
+    f_init_table_ << GetInsertToFinalTable(table_name, temporary_edges_table, vertex_id_mapping_table) << std::endl;
+    f_init_table_ << GetDropTable(temporary_edges_table) << std::endl;
+    f_init_table_ << GetDropTable(vertex_id_mapping_table) << std::endl;
+    f_init_table_ << add_length_column << std::endl;
+    f_init_table_ << fill_length_column << std::endl;
+    f_init_table_ << create_index << std::endl;
+}
 
-    void CopyWriter::WriteEdge(const string &table_name, const Edge &edge) {
-        // Write edge to data file. Data format is CSV with ';' delimiters.
-        string data = edge.get_osm_id() + "; " + edge.get_uid() + "; " + edge.get_geography() + "; " + edge.get_from() + "; " + edge.get_to();
-        f_data_ << data << std::endl;
-    }
+void CopyWriter::WriteEdge(const string &table_name, const Edge &edge) {
+    // Write edge to data file. Data format is CSV with ';' delimiters.
+    string data = edge.get_osm_id() + "; " + edge.get_uid() + "; " + edge.get_geography() + "; " + edge.get_from() + "; " + edge.get_to();
+    f_data_ << data << std::endl;
+}
 
-    void CopyWriter::WriteFinishSql(const std::string &table_name) {
-        // All done in WriteInitSql...
-    }
+void CopyWriter::WriteFinishSql(const std::string &table_name) {
+    // All done in WriteInitSql...
+}
 
-    std::string CopyWriter::GetCreateEdgesTable(const std::string& table_name) const {
-        return GetDropTable(table_name) + 
-            "CREATE TABLE " + table_name + "("  \
-            "   osm_id BIGINT NOT NULL, " \
-            "   uid BIGINT PRIMARY KEY, " \
-            "   geog geography(LINESTRING) NOT NULL, " \
-            "   from_node BIGINT NOT NULL, " \
-            "   to_node BIGINT NOT NULL " \
-            "); ";
-    }
+std::string CopyWriter::GetCreateEdgesTable(const std::string& table_name) const {
+    return GetDropTable(table_name) + 
+        "CREATE TABLE " + table_name + "("  \
+        "   osm_id BIGINT NOT NULL, " \
+        "   uid BIGINT PRIMARY KEY, " \
+        "   geog geography(LINESTRING) NOT NULL, " \
+        "   from_node BIGINT NOT NULL, " \
+        "   to_node BIGINT NOT NULL " \
+        "); ";
+}
 
-    std::string CopyWriter::GetCreateVertexIdMappingTable(const std::string& edges_table, const std::string& mapping_table) const {
-        return GetDropTable(mapping_table) + 
-            "CREATE TABLE " + mapping_table + " ( " \
-            "   osm_id BIGINT PRIMARY KEY, " \
-            "   new_id BIGSERIAL NOT NULL); " \
-            "INSERT INTO " + mapping_table + "(osm_id)( " \
-            "   SELECT from_node as vertex_id " \
-            "   FROM " + edges_table + " " \
-            "   UNION " \
-            "   SELECT to_node " \
-            "   FROM " + edges_table + " " \
-            "); ";
-    }
+std::string CopyWriter::GetCreateVertexIdMappingTable(const std::string& edges_table, const std::string& mapping_table) const {
+    return GetDropTable(mapping_table) + 
+        "CREATE TABLE " + mapping_table + " ( " \
+        "   osm_id BIGINT PRIMARY KEY, " \
+        "   new_id BIGSERIAL NOT NULL); " \
+        "INSERT INTO " + mapping_table + "(osm_id)( " \
+        "   SELECT from_node as vertex_id " \
+        "   FROM " + edges_table + " " \
+        "   UNION " \
+        "   SELECT to_node " \
+        "   FROM " + edges_table + " " \
+        "); ";
+}
 
-    std::string CopyWriter::GetInsertToFinalTable(const std::string& final_table, const std::string& edges_table, const std::string& mapping_table) const {
-        return "INSERT INTO " + final_table + "(osm_id, uid, geog, from_node, to_node) " \
-            " ( " \
-            "   SELECT e.osm_id, e.uid, e.geog, vfrom.new_id, vto.new_id " \
-            "   FROM " + edges_table + " AS e INNER JOIN " + mapping_table + " AS vfrom ON from_node = vfrom.osm_id " \
-            "   INNER JOIN " + mapping_table + " AS vto ON to_node = vto.osm_id " \
-            " ); ";
-    }
+std::string CopyWriter::GetInsertToFinalTable(const std::string& final_table, const std::string& edges_table, const std::string& mapping_table) const {
+    return "INSERT INTO " + final_table + "(osm_id, uid, geog, from_node, to_node) " \
+        " ( " \
+        "   SELECT e.osm_id, e.uid, e.geog, vfrom.new_id, vto.new_id " \
+        "   FROM " + edges_table + " AS e INNER JOIN " + mapping_table + " AS vfrom ON from_node = vfrom.osm_id " \
+        "   INNER JOIN " + mapping_table + " AS vto ON to_node = vto.osm_id " \
+        " ); ";
+}
 
-    std::string CopyWriter::GetDropTable(const std::string& table_name) const {
-        return "DROP TABLE IF EXISTS " + table_name + "; ";
-    }
+std::string CopyWriter::GetDropTable(const std::string& table_name) const {
+    return "DROP TABLE IF EXISTS " + table_name + "; ";
+}
+
+
 
 }
