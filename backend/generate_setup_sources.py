fb17f91dc8e0b7fdc2f30073e60e5480d5a163da
Krystof Hruby <hrubyk97@gmail.com>
2021-07-19 21:19:46 +0200

    Delete useless files.

--- backend/generate_setup_sources.py
+++
@@ -1,7 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-import glob
-sources = glob.glob('./src/routing/**/*.cpp', recursive=True)
-sources += glob.glob('./src/database/**/*.cpp', recursive=True)
-sources += glob.glob('./src/utility/**/*.cpp', recursive=True)
-print(sources)
